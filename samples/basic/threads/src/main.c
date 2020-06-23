b63e5847178a67c6e125666454a40b89b47aae1f
Eugeniy Paltsev <Eugeniy.Paltsev@synopsys.com>
2020-06-23 23:33:18 +0300

    samples/basic/threads: don't use GPIO so it can be run in nSIM

--- samples/basic/threads/src/main.c
+++ samples/basic/threads/src/main.c
@@ -6,7 +6,6 @@
 
 #include <zephyr.h>
 #include <device.h>
-#include <drivers/gpio.h>
 #include <sys/printk.h>
 #include <sys/__assert.h>
 #include <string.h>
@@ -17,19 +16,6 @@
 /* scheduling priority used by each thread */
 #define PRIORITY 7
 
-#define LED0_NODE DT_ALIAS(led0)
-#define LED1_NODE DT_ALIAS(led1)
-
-/*
- * Devicetree helper macro which gets the 'flags' cell from a 'gpios'
- * property, or returns 0 if the property has no 'flags' cell.
- */
-
-#define FLAGS_OR_ZERO(node)						\
-	COND_CODE_1(DT_PHA_HAS_CELL(node, gpios, flags),		\
-		    (DT_GPIO_FLAGS(node, gpios)),			\
-		    (0))
-
 struct printk_data_t {
 	void *fifo_reserved; /* 1st word reserved for use by fifo */
 	uint32_t led;
@@ -38,36 +24,11 @@ struct printk_data_t {
 
 K_FIFO_DEFINE(printk_fifo);
 
-struct led {
-	const char *gpio_dev_name;
-	const char *gpio_pin_name;
-	unsigned int gpio_pin;
-	unsigned int gpio_flags;
-};
-
-void blink(const struct led *led, uint32_t sleep_ms, uint32_t id)
+void blink(uint32_t sleep_ms, uint32_t id)
 {
-	struct device *gpio_dev;
 	int cnt = 0;
-	int ret;
-
-	gpio_dev = device_get_binding(led->gpio_dev_name);
-	if (gpio_dev == NULL) {
-		printk("Error: didn't find %s device\n",
-		       led->gpio_dev_name);
-		return;
-	}
-
-	ret = gpio_pin_configure(gpio_dev, led->gpio_pin, led->gpio_flags);
-	if (ret != 0) {
-		printk("Error %d: failed to configure pin %d '%s'\n",
-			ret, led->gpio_pin, led->gpio_pin_name);
-		return;
-	}
 
 	while (1) {
-		gpio_pin_set(gpio_dev, led->gpio_pin, cnt % 2);
-
 		struct printk_data_t tx_data = { .led = id, .cnt = cnt };
 
 		size_t size = sizeof(struct printk_data_t);
@@ -85,34 +46,15 @@ void blink(const struct led *led, uint32_t sleep_ms, uint32_t id)
 
 void blink0(void)
 {
-	const struct led led0 = {
-#if DT_NODE_HAS_STATUS(LED0_NODE, okay)
-		.gpio_dev_name = DT_GPIO_LABEL(LED0_NODE, gpios),
-		.gpio_pin_name = DT_LABEL(LED0_NODE),
-		.gpio_pin = DT_GPIO_PIN(LED0_NODE, gpios),
-		.gpio_flags = GPIO_OUTPUT | FLAGS_OR_ZERO(LED0_NODE),
-#else
-#error "Unsupported board: led0 devicetree alias is not defined"
-#endif
-	};
+	printk("  PAL: %s: %d\n", __func__, __LINE__);
 
-	blink(&led0, 100, 0);
+	blink(500, 0);
 }
 
 void blink1(void)
 {
-	const struct led led1 = {
-#if DT_NODE_HAS_STATUS(LED1_NODE, okay)
-		.gpio_dev_name = DT_GPIO_LABEL(LED1_NODE, gpios),
-		.gpio_pin_name = DT_LABEL(LED1_NODE),
-		.gpio_pin = DT_GPIO_PIN(LED1_NODE, gpios),
-		.gpio_flags = GPIO_OUTPUT | FLAGS_OR_ZERO(LED1_NODE),
-#else
-#error "Unsupported board: led1 devicetree alias is not defined"
-#endif
-	};
-
-	blink(&led1, 1000, 1);
+	printk("  PAL: %s: %d\n", __func__, __LINE__);
+	blink(1000, 1);
 }
 
 void uart_out(void)
