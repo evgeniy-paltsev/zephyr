c606073d907761c509aead6ea768bd54ac67872f
Eugeniy Paltsev <Eugeniy.Paltsev@synopsys.com>
2020-06-29 22:31:21 +0300

    ARCv3 64: [hack] allow sanitycheck run for ARCv3 (nsim_hs_nirq)

--- cmake/emu/nsim.cmake
+++ cmake/emu/nsim.cmake
@@ -15,6 +15,17 @@ elseif(${CONFIG_SOC_NSIM_HS_NIRQ})
  set(NSIM_PROPS nsim_hs_nirq.props)
 endif()
 
+if(${CONFIG_SOC_NSIM_HS_NIRQ})
+add_custom_target(run
+  COMMAND
+  mdb -arc64 -OK -OKN -nogoifmain -Xatomic -Xtimer0 -Xtimer1 -Xdiv_rem -prop=nsim_isa_addr_size=64 -prop=nsim_isa_pc_size=64 -prop=nsim_mem-dev=uart0,base=0xf0000000,irq=24 -cl -run
+  ${APPLICATION_BINARY_DIR}/zephyr/${KERNEL_ELF_NAME}
+  DEPENDS ${logical_target_for_zephyr_elf}
+  WORKING_DIRECTORY ${APPLICATION_BINARY_DIR}
+  USES_TERMINAL
+  )
+
+else()
 add_custom_target(run
   COMMAND
   ${NSIM}
@@ -25,6 +36,8 @@ add_custom_target(run
   WORKING_DIRECTORY ${APPLICATION_BINARY_DIR}
   USES_TERMINAL
   )
+
+endif()
 
 add_custom_target(debugserver
   COMMAND
