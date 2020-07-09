30e2f69c5c1cd06b03b08835ca41ed3434de70d6
Eugeniy Paltsev <Eugeniy.Paltsev@synopsys.com>
2020-07-10 02:56:35 +0300

    ARCv3 64: dump full 64 bit register content

--- arch/arc/core/fatal.c
+++ arch/arc/core/fatal.c
@@ -24,16 +24,16 @@ LOG_MODULE_DECLARE(os);
 #ifdef CONFIG_ARC_EXCEPTION_DEBUG
 static void dump_arc_esf(const z_arch_esf_t *esf)
 {
-	LOG_ERR(" r0: 0x%zx  r1: 0x%zx  r2: 0x%zx  r3: 0x%zx",
+	LOG_ERR(" r0: 0x%" PRIxPTR "  r1: 0x%" PRIxPTR "  r2: 0x%" PRIxPTR "  r3: 0x%" PRIxPTR "",
 		esf->r0, esf->r1, esf->r2, esf->r3);
-	LOG_ERR(" r4: 0x%zx  r5: 0x%zx  r6: 0x%zx  r7: 0x%zx",
+	LOG_ERR(" r4: 0x%" PRIxPTR "  r5: 0x%" PRIxPTR "  r6: 0x%" PRIxPTR "  r7: 0x%" PRIxPTR "",
 		esf->r4, esf->r5, esf->r6, esf->r7);
-	LOG_ERR(" r8: 0x%zx  r9: 0x%zx r10: 0x%zx r11: 0x%zx",
+	LOG_ERR(" r8: 0x%" PRIxPTR "  r9: 0x%" PRIxPTR " r10: 0x%" PRIxPTR " r11: 0x%" PRIxPTR "",
 		esf->r8, esf->r9, esf->r10, esf->r11);
-	LOG_ERR("r12: 0x%zx r13: 0x%zx  pc: 0x%zx",
+	LOG_ERR("r12: 0x%" PRIxPTR " r13: 0x%" PRIxPTR "  pc: 0x%" PRIxPTR "",
 		esf->r12, esf->r13, esf->pc);
-	LOG_ERR(" blink: 0x%zx status32: 0x%zx", esf->blink, esf->status32);
-	LOG_ERR("lp_end: 0x%zx lp_start: 0x%zx lp_count: 0x%zx",
+	LOG_ERR(" blink: 0x%" PRIxPTR " status32: 0x%" PRIxPTR "", esf->blink, esf->status32);
+	LOG_ERR("lp_end: 0x%" PRIxPTR " lp_start: 0x%" PRIxPTR " lp_count: 0x%" PRIxPTR "",
 		esf->lp_end, esf->lp_start, esf->lp_count);
 }
 #endif
