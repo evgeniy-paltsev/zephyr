386d33f260ddd5432acdab35afa28950c2782fe7
Evgeniy Paltsev <PaltsevEvgeniy@gmail.com>
2021-09-29 16:50:52 +0300

    ARC: setup interrupt priority of static irq at init time
    Signed-off-by: Evgeniy Paltsev <PaltsevEvgeniy@gmail.com>

--- drivers/interrupt_controller/intc_arcv2_irq_unit.c
+++ drivers/interrupt_controller/intc_arcv2_irq_unit.c
@@ -45,6 +45,42 @@ struct arc_v2_irq_unit_ctx {
 static struct arc_v2_irq_unit_ctx ctx;
 #endif
 
+
+#ifdef CONFIG_ARC_SECURE_FIRMWARE
+/* lowest priority */
+#define DEFAULT_IRQ_PRIO ((CONFIG_NUM_IRQ_PRIO_LEVELS - 1) | _ARC_V2_IRQ_PRIORITY_SECURE)
+#else
+/* lowest priority */
+#define DEFAULT_IRQ_PRIO (CONFIG_NUM_IRQ_PRIO_LEVELS - 1)
+#endif
+
+extern char __arc_isr_static_start[];
+extern char __arc_isr_static_end[];
+
+static void arc_v2_irq_init_prio(void)
+{
+	struct arc_isr_sratic_data *start = (struct arc_isr_sratic_data *)__arc_isr_static_start;
+	struct arc_isr_sratic_data *end = (struct arc_isr_sratic_data *)__arc_isr_static_end;
+
+	__ASSERT(start < end, "no static irq configuration");
+	__ASSERT(start->irq >= CONFIG_GEN_IRQ_START_VECTOR, "incorrect static irq number");
+
+	/*
+	 * If interrupt is connected in build-time extract priority from
+	 * static configuration.
+	 */
+	for (uint32_t irq = CONFIG_GEN_IRQ_START_VECTOR; irq < CONFIG_NUM_IRQS; irq++) {
+		if (irq == start->irq && start < end) {
+			/* TODO: get rid of z_irq_priority_set usage - as it locks interrupts */
+			z_irq_priority_set(irq, start->priority, 0);
+			start++;
+		} else {
+			z_arc_v2_aux_reg_write(_ARC_V2_IRQ_SELECT, irq);
+			z_arc_v2_aux_reg_write(_ARC_V2_IRQ_PRIORITY, DEFAULT_IRQ_PRIO);
+		}
+	}
+}
+
 /*
  * @brief Initialize the interrupt unit device driver
  *
@@ -62,21 +98,17 @@ static int arc_v2_irq_unit_init(const struct device *unused)
 	ARG_UNUSED(unused);
 	int irq; /* the interrupt index */
 
+	arc_v2_irq_init_prio();
+
 	/* Interrupts from 0 to 15 are exceptions and they are ignored
 	 * by IRQ auxiliary registers. For that reason we skip those
 	 * values in this loop.
 	 */
-	for (irq = 16; irq < CONFIG_NUM_IRQS; irq++) {
+	BUILD_ASSERT(CONFIG_GEN_IRQ_START_VECTOR == 16);
+
+	for (irq = CONFIG_GEN_IRQ_START_VECTOR; irq < CONFIG_NUM_IRQS; irq++) {
 
 		z_arc_v2_aux_reg_write(_ARC_V2_IRQ_SELECT, irq);
-#ifdef CONFIG_ARC_SECURE_FIRMWARE
-		z_arc_v2_aux_reg_write(_ARC_V2_IRQ_PRIORITY,
-			 (CONFIG_NUM_IRQ_PRIO_LEVELS-1) |
-			 _ARC_V2_IRQ_PRIORITY_SECURE); /* lowest priority */
-#else
-		z_arc_v2_aux_reg_write(_ARC_V2_IRQ_PRIORITY,
-			 (CONFIG_NUM_IRQ_PRIO_LEVELS-1)); /* lowest priority */
-#endif
 		z_arc_v2_aux_reg_write(_ARC_V2_IRQ_ENABLE, _ARC_V2_INT_DISABLE);
 		z_arc_v2_aux_reg_write(_ARC_V2_IRQ_TRIGGER, _ARC_V2_INT_LEVEL);
 	}
