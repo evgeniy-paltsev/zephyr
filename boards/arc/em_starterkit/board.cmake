12d5d2f50d991bce8ec24c6332542125024c8343
Evgeniy Paltsev <PaltsevEvgeniy@gmail.com>
2021-10-25 15:42:45 +0300

    ARC: EMSK: disable MPU with openocd
    Signed-off-by: Evgeniy Paltsev <PaltsevEvgeniy@gmail.com>

--- boards/arc/em_starterkit/board.cmake
+++ boards/arc/em_starterkit/board.cmake
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: Apache-2.0
 
 board_runner_args(openocd "--use-elf")
+#if(CONFIG_CPU_HAS_MPU)
+board_runner_args(openocd --cmd-pre-load "arc jtag set-aux-reg 0x409 0")
 board_runner_args(mdb-hw "--jtag=digilent")
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/mdb-hw.board.cmake)
