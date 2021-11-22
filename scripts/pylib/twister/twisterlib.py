5d875e1e7abb72edf854e52e1ad5892245812330
Evgeniy Paltsev <PaltsevEvgeniy@gmail.com>
2021-11-23 01:02:22 +0300

    twister: implement platform-specific test timeout management
    Twister allows us to control maximum execution time for each
    test with timeout value in test .yaml configuration. This
    helps us to prevent slow tests from stopping by timeout.
    
    However it's hard to choose test timeout for all platforms
    as some platforms are naturally slow. It could be a HW board with
    power-efficient but slow CPU or simulation platform which
    can perform instruction accurate simulation but does it slowly.
    
    As we don't want to increase test timeout infinitely to meet
    the needs of the slowest platform let's introduce
    platform-specific test timeout management. It's implemented as
    an optional 'timeout_multiplier' field in board .yaml
    configuration. Setting it to some value multiplies each test
    timeout by this value. By that we can increase timeouts only
    for the platforms where it's required.
    
    Signed-off-by: Eugeniy Paltsev <Eugeniy.Paltsev@synopsys.com>
    Signed-off-by: Evgeniy Paltsev <PaltsevEvgeniy@gmail.com>

--- scripts/pylib/twister/twisterlib.py
+++ scripts/pylib/twister/twisterlib.py
@@ -7,6 +7,7 @@ import os
 import contextlib
 import string
 import mmap
+import math
 import sys
 import re
 import subprocess
@@ -397,7 +398,7 @@ class Handler:
 
         self.name = instance.name
         self.instance = instance
-        self.timeout = instance.testcase.timeout
+        self.timeout = math.ceil(instance.testcase.timeout * instance.platform.timeout_multiplier)
         self.sourcedir = instance.testcase.source_dir
         self.build_dir = instance.build_dir
         self.log = os.path.join(self.build_dir, "handler.log")
@@ -1554,6 +1555,7 @@ class Platform:
         # if no RAM size is specified by the board, take a default of 128K
         self.ram = 128
 
+        self.timeout_multiplier = 1.0
         self.ignore_tags = []
         self.only_tags = []
         self.default = False
@@ -1579,6 +1581,7 @@ class Platform:
         # if no RAM size is specified by the board, take a default of 128K
         self.ram = data.get("ram", 128)
         testing = data.get("testing", {})
+        self.timeout_multiplier = testing.get("timeout_multiplier", 1.0)
         self.ignore_tags = testing.get("ignore_tags", [])
         self.only_tags = testing.get("only_tags", [])
         self.default = testing.get("default", False)
