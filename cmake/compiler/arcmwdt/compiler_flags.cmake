5e85a893f1fcd5aa0357f0fa9064ef9548e2f0bc
Evgeniy Paltsev <PaltsevEvgeniy@gmail.com>
2023-02-09 19:14:01 +0000

    Enable TLS with MWDT toolchain
    Signed-off-by: Evgeniy Paltsev <PaltsevEvgeniy@gmail.com>

--- cmake/compiler/arcmwdt/compiler_flags.cmake
+++ cmake/compiler/arcmwdt/compiler_flags.cmake
@@ -149,13 +149,7 @@ set_property(TARGET compiler-cpp PROPERTY no_rtti "-fno-rtti")
 set_compiler_property(PROPERTY freestanding -Hnocrt)
 
 # Flag to enable debugging
-if(CONFIG_THREAD_LOCAL_STORAGE)
-  # FIXME: Temporary workaround for ARC MWDT toolchain issue - LLDAC linker produce errors on
-  # debugging information (if -g option specified) of thread-local variables.
-  set_compiler_property(PROPERTY debug)
-else()
-  set_compiler_property(PROPERTY debug -g)
-endif()
+set_compiler_property(PROPERTY debug -g)
 
 # compile common globals like normal definitions
 set_compiler_property(PROPERTY no_common -fno-common)
